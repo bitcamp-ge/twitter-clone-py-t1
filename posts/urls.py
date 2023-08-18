091ed0bcb4cbeec183293a0900d8d825fe93b74e
Zoia Tsitsilashvili <zoika.leli@gmail.com>
2023-08-19 00:34:51 +0400

    #2 change post/urls

--- posts/urls.py
+++ posts/urls.py
@@ -1,14 +1,21 @@
-from django.urls import path, include
-from rest_framework import routers
-from . views import PostView
+# from django.urls import path, include
+# from rest_framework import routers
+# from . views import PostListCreateView, PostDetailView
 
+# router  = routers.DefaultRouter()
+# router.register(r'posts', PostListCreateView, 'post')
+# router.register(r'posts', PostDetailView)
 
+# urlpatterns = [
+#     path('', include(router.urls)),  
+# ]
 
-router  = routers.DefaultRouter()
-router.register(r'posts', PostView, 'post')
+from django.urls import path
+from .views import PostListCreateView, PostDetailView
 
+app_name = 'posts'
 
 urlpatterns = [
-    path('', include(router.urls)),
-    
+    path('posts/', PostListCreateView.as_view(), name='post-list'),
+    path('<int:pk>/', PostDetailView.as_view(), name='post-detail'),
 ]
