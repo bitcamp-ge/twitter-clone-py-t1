8f28988f1c03a23d738cad3ba1dd8997024006f9
unknown <khupeniasaba@gmail.com>
2023-08-18 11:32:03 +0400

    #6-add-comments

---
+++ comments/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+from django.contrib.contenttypes.fields import GenericForeignKey, ContentType
+from django.contrib.auth.models import User
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
