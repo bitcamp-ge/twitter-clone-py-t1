2ec47e6a037e307a49eb924db1dc8b6b79c8bfaa
Zoia Tsitsilashvili <zoika.leli@gmail.com>
2023-08-19 00:33:47 +0400

    #2 add TokenAuthentication

--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,24 @@
 from rest_framework import viewsets
+from rest_framework import generics, permissions
+from rest_framework.authentication import TokenAuthentication
 from .models import Post
 from .serializers import PostSerializer
+from .permissions import IsOwnerOrReadOnly
 
 
-
-class PostView(viewsets.ModelViewSet):
+class PostListCreateView(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+
+class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
