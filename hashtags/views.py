d530fff9622930bf0d358796a16d687c38c915ed
Zoia Tsitsilashvili <zoika.leli@gmail.com>
2023-08-18 15:32:47 +0400

    #15 add hashtagViews

---
+++ hashtags/views.py
@@ -0,0 +1,8 @@
+# from rest_framework import viewsets
+# from .models import Hashtag
+# from .serializers import HashtagSerializer
+
+
+# class HashtagView(viewsets.ModelViewSet):
+#     queryset = Hashtag.objects.all()
+#     serializer_class = HashtagSerializer
