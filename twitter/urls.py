34345c3b9c443928099c7c41fdacb749727eaa25
Zoia Tsitsilashvili <zoika.leli@gmail.com>
2023-08-18 15:19:22 +0400

    #10 update twitter-urls

--- twitter/urls.py
+++ twitter/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("signup/", views.signup),
     path("login/", views.login),
-    path("test/", views.test)
+    path("test/", views.test),
+    path('', include('hashtags.urls')),
+
+    
 ]
