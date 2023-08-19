1e1f088f4e61b6b99627d38102fcdfa313231843
Zoia Tsitsilashvili <zoika.leli@gmail.com>
2023-08-19 16:14:37 +0400

    #2 change views-code, Returns the name of the hashtags

--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,9 @@
-from rest_framework import viewsets
 from rest_framework import generics, permissions
 from rest_framework.authentication import TokenAuthentication
 from .models import Post
 from .serializers import PostSerializer
 from .permissions import IsOwnerOrReadOnly
-
+from hashtags.models import Hashtag
 
 class PostListCreateView(generics.ListCreateAPIView):
     queryset = Post.objects.all()
@@ -15,6 +14,14 @@ class PostListCreateView(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
+    def perform_create(self, serializer):
+        hashtags_data = self.request.data.get('hashtags')  # Get the hashtag IDs from the request
+        post = serializer.save(user=self.request.user)
+
+        if hashtags_data:
+            for hashtag_id in hashtags_data:
+                hashtag = Hashtag.objects.get(pk=hashtag_id)
+                post.hashtags.add(hashtag)  # Associate the hashtags with the post
 
 
 class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
