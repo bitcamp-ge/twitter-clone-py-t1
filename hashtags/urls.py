335a8584df6da7cd1164f6aa819f06414624bfc5
Zoia Tsitsilashvili <zoika.leli@gmail.com>
2023-08-19 16:09:44 +0400

    #15 change hashtags/urls

--- hashtags/urls.py
+++ hashtags/urls.py
@@ -1,16 +1,7 @@
-from django.urls import path, include
-from rest_framework import routers
-from . views import HashtagView
-# from posts.views import PostListCreateView
-
-
-
-router  = routers.DefaultRouter()
-router.register(r'hashtags', HashtagView)
-# router.register(r'posts', PostListCreateView)
-
-
+from django.urls import path
+from .views import HashtagListCreateView, HashtagDetailView
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('hashtags/', HashtagListCreateView.as_view(), name='hashtag-list'),
+    path('hashtags/<int:pk>/', HashtagDetailView.as_view(), name='hashtag-detail'),
 ]
