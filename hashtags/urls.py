0cb9b72485052e7ffdd6a3b2716745c230c46dcd
Zoia Tsitsilashvili <zoika.leli@gmail.com>
2023-08-19 18:30:37 +0400

    #10 hashtag.urls

--- hashtags/urls.py
+++ hashtags/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import HashtagListCreateView, HashtagDetailView
 
+
+
 urlpatterns = [
     path('hashtags/', HashtagListCreateView.as_view(), name='hashtag-list'),
     path('hashtags/<int:pk>/', HashtagDetailView.as_view(), name='hashtag-detail'),
