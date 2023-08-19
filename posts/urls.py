2863395cf490b4da37ddfab089a95620d7a44a50
Zoia Tsitsilashvili <zoika.leli@gmail.com>
2023-08-19 16:19:52 +0400

    #2 urls/Nothing has changed

--- posts/urls.py
+++ posts/urls.py
@@ -1,15 +1,3 @@
-# from django.urls import path, include
-# from rest_framework import routers
-# from . views import PostListCreateView, PostDetailView
-
-# router  = routers.DefaultRouter()
-# router.register(r'posts', PostListCreateView, 'post')
-# router.register(r'posts', PostDetailView)
-
-# urlpatterns = [
-#     path('', include(router.urls)),  
-# ]
-
 from django.urls import path
 from .views import PostListCreateView, PostDetailView
 
