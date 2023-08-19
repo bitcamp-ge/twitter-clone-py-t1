7897d72a768d269ca22e8b3a6884026e0a31683b
Zoia Tsitsilashvili <zoika.leli@gmail.com>
2023-08-19 18:31:12 +0400

    #10 hashtag.views

--- hashtags/views.py
+++ hashtags/views.py
@@ -2,9 +2,12 @@ from rest_framework import generics
 from .models import Hashtag
 from .serializers import HashtagSerializer
 
+
+
 class HashtagListCreateView(generics.ListCreateAPIView):
     queryset = Hashtag.objects.all()
     serializer_class = HashtagSerializer
+
 
 class HashtagDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Hashtag.objects.all()
