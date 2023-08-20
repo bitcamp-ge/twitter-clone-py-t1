ac5c96e190f5b2db66dd0d9839120ce664b257a5
Zoia Tsitsilashvili <zoika.leli@gmail.com>
2023-08-20 14:11:31 +0400

    #2 add posts

---
+++ posts/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Post
+from hashtags.serializers import HashtagSerializer
+
+
+class PostSerializer(serializers.ModelSerializer):
+    hashtags = HashtagSerializer(many=True, read_only=True, required=False)
+
+
+    class Meta:
+        model = Post
+        fields = ["title", "content", "hashtags"]
+
+  
