277ed4224083c9dee6494fdae9988a7dfabe37e4
NickKipshidze <kipshidze.nick@gmail.com>
2023-08-20 19:51:00 +0400

    #14 Add accounts app

--- twitter/settings.py
+++ twitter/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'corsheaders',
+    
+    'accounts',
     'hashtags',
     'posts',
     
@@ -91,6 +93,8 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
+
+AUTH_USER_MODEL = "accounts.User"
 
 AUTH_PASSWORD_VALIDATORS = [
     {
