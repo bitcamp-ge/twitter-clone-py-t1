b9b3b17b904ea1955ea11be2f09a88b5e1410d16
NickKipshidze <kipshidze.nick@gmail.com>
2023-08-20 20:06:33 +0400

    #14 Add user profiles

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("login", views.login, name="login"),
-    path("signup", views.signup, name="signup"),
-    path("test", views.test)
+    path("login", views.LoginView.as_view(), name="login"),
+    path("signup", views.SignupView.as_view(), name="signup"),
+    path("profile/<str:username>", views.ProfileView.as_view(), name="profile")
 ]
