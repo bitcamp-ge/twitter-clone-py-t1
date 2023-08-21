af7dc31f485318b8d51c21810fda9a1b17f40ba5
Zoia Tsitsilashvili <zoika.leli@gmail.com>
2023-08-21 17:09:12 +0400

    #2 change post.serializers/delete field-'title'

--- posts/serializers.py
+++ posts/serializers.py
@@ -9,6 +9,6 @@ class PostSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Post
-        fields = ["title", "content", "hashtags"]
+        fields = ["content", "hashtags"]
 
   
