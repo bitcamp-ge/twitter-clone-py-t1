ab30c0dabcd724cae6a128339e93599c0117addd
Zoia Tsitsilashvili <zoika.leli@gmail.com>
2023-08-21 17:07:42 +0400

    #2 change post/model/hashtags.field

--- posts/models.py
+++ posts/models.py
@@ -6,5 +6,5 @@ class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     content = models.TextField()
-    hashtags = models.ManyToManyField(Hashtag, related_name='posts')
+    hashtags = models.ManyToManyField(Hashtag, related_name='posts', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
