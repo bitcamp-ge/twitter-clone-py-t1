9e9ce58f8faf0d09b3ec3f3b82bc0797cd24b4d5
Zoia Tsitsilashvili <zoika.leli@gmail.com>
2023-08-22 22:41:52 +0400

    #2 change serializers- Post/fields

--- posts/serializers.py
+++ posts/serializers.py
@@ -6,10 +6,9 @@ from hashtags.serializers import HashtagSerializer
 class PostSerializer(serializers.ModelSerializer):
     hashtags = HashtagSerializer(many=True, read_only=True, required=False)
 
+
     class Meta:
         model = Post
-        fields = ["title", "content", "hashtags"]
+        fields = ["content", "hashtags"]
 
-    
-
-    
+  
