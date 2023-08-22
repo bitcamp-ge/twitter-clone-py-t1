2b56e385e2a837840fc0b16eaaeca1e24f20acdf
Zoia Tsitsilashvili <zoika.leli@gmail.com>
2023-08-22 22:08:43 +0400

    #2 change hashtag/views Used viewsets instead of APIView The old code is commented out for comparison

--- hashtags/views.py
+++ hashtags/views.py
@@ -1,14 +1,21 @@
-from rest_framework import generics
+# from rest_framework import generics
+# from .models import Hashtag
+# from .serializers import HashtagSerializer
+
+# class HashtagListCreateView(generics.ListCreateAPIView):
+#     queryset = Hashtag.objects.all()
+#     serializer_class = HashtagSerializer
+
+# class HashtagDetailView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Hashtag.objects.all()
+#     serializer_class = HashtagSerializer
+
+
+from rest_framework import viewsets
 from .models import Hashtag
 from .serializers import HashtagSerializer
 
 
-
-class HashtagListCreateView(generics.ListCreateAPIView):
-    queryset = Hashtag.objects.all()
-    serializer_class = HashtagSerializer
-
-
-class HashtagDetailView(generics.RetrieveUpdateDestroyAPIView):
+class HashtagViewSet(viewsets.ModelViewSet):
     queryset = Hashtag.objects.all()
     serializer_class = HashtagSerializer
