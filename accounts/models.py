8877e1c7685238501e0f3f3bd4f671a2dd3c123e
NickKipshidze <kipshidze.nick@gmail.com>
2023-08-22 14:32:02 +0400

    #14 Add bio field

--- accounts/models.py
+++ accounts/models.py
@@ -4,5 +4,7 @@ from django.db import models
 # Create your models here.
 
 class User(AbstractUser):
+    bio = models.CharField(max_length=128, null=True)
+    
     def __str__(self):
         return self.username
