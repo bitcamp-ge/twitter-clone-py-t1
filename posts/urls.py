80db8147574f25e0a3b7f5475d3c80b8f8536380
Zoia Tsitsilashvili <zoika.leli@gmail.com>
2023-08-22 22:46:26 +0400

    #2 change posts.urls import DefaultRouter, add HashtagViewSet router configuration. The old code is commented out for comparison

--- posts/urls.py
+++ posts/urls.py
@@ -1,9 +1,23 @@
-from django.urls import path
-from .views import PostListCreateView, PostDetailView
+# from django.urls import path
+# from .views import PostListCreateView, PostDetailView
 
-app_name = 'posts'
+
+# app_name = 'posts'
+
+# urlpatterns = [
+#     path('posts/', PostListCreateView.as_view(), name='post-list'),
+#     path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+# ]
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import PostViewSet
+
+
+router = DefaultRouter()
+router.register(r'posts', PostViewSet)
+
 
 urlpatterns = [
-    path('posts/', PostListCreateView.as_view(), name='post-list'),
-    path('<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('', include(router.urls)),
 ]
