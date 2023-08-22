61abd8a15b202f7d9365a4c96c5c30174b4f474e
NickKipshidze <kipshidze.nick@gmail.com>
2023-08-22 14:32:39 +0400

    #14 Add URL for setting user bio

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("login", views.LoginView.as_view(), name="login"),
     path("signup", views.SignupView.as_view(), name="signup"),
-    path("profile/<str:username>", views.ProfileView.as_view(), name="profile")
+    path("profile/<str:username>", views.ProfileView.as_view(), name="profile"),
+    path("profile/set/bio", views.SetBioView.as_view(), name="setbio")
 ]
