dd7f85d4dcb41538121755a11276f92becb2a6df
Zoia Tsitsilashvili <zoika.leli@gmail.com>
2023-08-22 22:18:05 +0400

    #2 change hashtag.urls import DefaultRouter HashtagViewSet router configuration The old code is #commented out fot comparison

--- hashtags/urls.py
+++ hashtags/urls.py
@@ -1,9 +1,22 @@
-from django.urls import path
-from .views import HashtagListCreateView, HashtagDetailView
+# from django.urls import path
+# from .views import HashtagListCreateView, HashtagDetailView
 
 
+# urlpatterns = [
+#     path('hashtags/', HashtagListCreateView.as_view(), name='hashtag-list'),
+#     path('hashtags/<int:pk>/', HashtagDetailView.as_view(), name='hashtag-detail'),
+# ]
+
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import HashtagViewSet
+from posts.views import PostViewSet
+
+router = DefaultRouter()
+router.register(r'hashtags', HashtagViewSet)
+router.register(r'posts', PostViewSet)
 
 urlpatterns = [
-    path('hashtags/', HashtagListCreateView.as_view(), name='hashtag-list'),
-    path('hashtags/<int:pk>/', HashtagDetailView.as_view(), name='hashtag-detail'),
+    path('', include(router.urls)),
 ]
