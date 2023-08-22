346fd4309a7d6e3dcfddeee59296794c36d68984
Zoia Tsitsilashvili <zoika.leli@gmail.com>
2023-08-22 22:34:47 +0400

    #2 change post.model - in hashtag field add blank=True

--- posts/models.py
+++ posts/models.py
@@ -1,11 +1,10 @@
 from django.db import models 
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User    # use from accounts.models import User
 from hashtags.models import Hashtag
-
 
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     content = models.TextField()
-    hashtags = models.ManyToManyField(Hashtag, related_name='posts')
+    hashtags = models.ManyToManyField(Hashtag, related_name='posts', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
