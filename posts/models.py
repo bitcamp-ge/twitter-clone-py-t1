19a426849952a878a51993be99e469e1ad14ad3e
Zoia Tsitsilashvili <zoika.leli@gmail.com>
2023-08-25 18:39:10 +0400

    #2 change post/models.py delete field 'title' add in field- hashags 'blank=True'

--- posts/models.py
+++ posts/models.py
@@ -4,7 +4,6 @@ from hashtags.models import Hashtag
 
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
     content = models.TextField()
-    hashtags = models.ManyToManyField(Hashtag, related_name='posts')
+    hashtags = models.ManyToManyField(Hashtag, related_name='posts', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
