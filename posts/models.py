06f1bc76dfa9064351bcc26b21409b2a97471cbb
nodarisvanidze88 <nodarisvanidze88@gmail.com>
2023-08-27 06:39:43 +1000

    Add in hashtags vlank value and string output function

--- posts/models.py
+++ posts/models.py
@@ -6,5 +6,8 @@ class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     content = models.TextField()
-    hashtags = models.ManyToManyField(Hashtag, related_name='posts')
+    hashtags = models.ManyToManyField(Hashtag, related_name='posts', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
