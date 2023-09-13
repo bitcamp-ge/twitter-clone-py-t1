358d332ff1c3faa4ca3a72b630d5ad3293c58646
Zoia Tsitsilashvili <zoika.leli@gmail.com>
2023-09-13 11:00:23 +0400

    #2 Adding likes and comments to the post serializer

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,11 +1,16 @@
 from rest_framework import serializers
 from .models import Post
 from hashtags.serializers import HashtagSerializer
+from comments.serializers import CommentSerializer
+from likes.serializers import LikeSerializer
+
 
 
 class PostSerializer(serializers.ModelSerializer):
     hashtags = HashtagSerializer(many=True, read_only=True, required=False)
-
+    comments = CommentSerializer(many=True, read_only=True)
+    likes = LikeSerializer(many=True, read_only=True)
+   
 
     class Meta:
         model = Post
