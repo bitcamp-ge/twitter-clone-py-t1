f23dc7888b6ab8fd9fa7242adcb4798a3a23231a
Zoia Tsitsilashvili <zoika.leli@gmail.com>
2023-09-13 16:56:32 +0400

    #2 Change: Added like and comment fields to post/models.py

--- posts/models.py
+++ posts/models.py
@@ -1,12 +1,16 @@
 from django.db import models 
 from accounts.models import User
 from hashtags.models import Hashtag
+from likes.models import Likes
+from comments.models import Comment
 
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     hashtags = models.ManyToManyField(Hashtag, related_name='posts', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, through=Likes, related_name='post_likes')
+    comments = models.ManyToManyField(Comment, related_name='post_comments', blank=True)
    
     def __str__(self):
         return self.content
